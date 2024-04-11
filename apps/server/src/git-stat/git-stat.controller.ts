63a0fc8efd67743f2499dc70c9fffdc7379da99d
YAN7 <904394373@qq.com>
2024-04-11 10:45:51 +0800

    feat(server): 完成设置/获取时间区间接口

--- apps/server/src/git-stat/git-stat.controller.ts
+++ apps/server/src/git-stat/git-stat.controller.ts
@@ -1,16 +1,24 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { readFileSync, writeFileSync } from 'fs';
 
 @Controller('git-stat')
 export class GitStatController {
   @Post('setQueryDate')
   wawa(@Body() body) {
-    console.log('body ==> ', body);
-    return '111';
+    const appConfig = JSON.parse(readFileSync('./date.config.json').toString());
+    appConfig.startDate = body.startDate;
+    appConfig.endDate = body.endDate;
+    writeFileSync(
+      './date.config.json',
+      JSON.stringify(appConfig, null, 2),
+      'utf-8',
+    );
   }
 
   @Get('queryDate')
   hehe() {
-    return '123';
+    const appConfig = JSON.parse(readFileSync('./date.config.json').toString());
+    return appConfig;
   }
 
   @Post('collect')
