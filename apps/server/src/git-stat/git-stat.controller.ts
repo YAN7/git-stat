20e11b7b02d903bd7b14b957712b2ae5f2a4175f
YAN7 <904394373@qq.com>
2024-04-16 16:58:36 +0800

    feat: 完成收集信息接口

--- apps/server/src/git-stat/git-stat.controller.ts
+++ apps/server/src/git-stat/git-stat.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { readdirSync, readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
+import { mkdir, readdir, readFile, writeFile } from 'fs/promises';
 
 @Controller('git-stat')
 export class GitStatController {
@@ -25,13 +26,79 @@ export class GitStatController {
     return appConfig;
   }
 
+  updateSubmitInfo(source, submitInfo) {
+    const { username, addLines, removeLines, totalLines } = submitInfo;
+    if (!source[username]) {
+      source[username] = {
+        projects: [submitInfo],
+        addLines,
+        removeLines,
+        totalLines,
+      };
+      return source;
+    }
+    const { projects } = source[username];
+    source[username].projects = this.updateProject(projects, submitInfo);
+    source[username] = this.updateTotalInfo(source[username]);
+    return source;
+  }
+
+  updateProject(projects, submitInfo) {
+    const { projectCode } = submitInfo;
+    let isProjectExist = false;
+    const newProject = projects.map((p) => {
+      if (p.projectCode === projectCode) {
+        isProjectExist = true;
+        return submitInfo;
+      }
+      return p;
+    });
+    if (isProjectExist) {
+      return newProject;
+    }
+    return [...newProject, submitInfo];
+  }
+
+  updateTotalInfo(source) {
+    if (source.projects?.length === 0) return source;
+    source.addLines = source.removeLines = source.totalLines = 0;
+    source.projects.reduce(
+      (prev, next) => {
+        source.addLines = source.addLines + next.addLines;
+        source.removeLines = source.removeLines + next.removeLines;
+        source.totalLines = source.totalLines + next.totalLines;
+        return next;
+      },
+      { addLines: 0, removeLines: 0, totalLines: 0 },
+    );
+    return source;
+  }
+
   @Post('collect')
-  lala(@Body() submitDto) {
-    const archive = readdirSync('./archive');
+  async lala(@Body() submitDto) {
+    const res = await readdir('./archive').catch((err) =>
+      console.log('err', err),
+    );
+    if (res === undefined) {
+      await mkdir('./archive');
+    }
     const { startDate, endDate } = submitDto;
-    const filename = `${startDate}-${endDate}.json`;
-    const file = readFileSync(`./archive1/${filename}`);
-    console.log('file ==> ', file.toString());
-    return '456';
+    submitDto.addLines = parseInt(submitDto.addLines || 0);
+    submitDto.removeLines = parseInt(submitDto.removeLines || 0);
+    submitDto.totalLines = parseInt(submitDto.totalLines || 0);
+    const filePath = `./archive/${startDate}-${endDate}.json`;
+    const file = await readFile(filePath).catch((err) =>
+      console.log('err', err),
+    );
+    if (!file) {
+      const newData = this.updateSubmitInfo({}, submitDto);
+      await writeFile(filePath, JSON.stringify(newData, null, 2));
+      return { status: 1, message: '保存成功' };
+    } else {
+      const currData = JSON.parse(file.toString());
+      const newData = this.updateSubmitInfo(currData, submitDto);
+      await writeFile(filePath, JSON.stringify(newData, null, 2));
+      return { status: 1, message: '保存成功' };
+    }
   }
 }
---
+++ apps/server/src/git-stat/git-stat.dto.ts
@@ -0,0 +1,11 @@
+export class GitStatInfo {
+  startDate: string;
+  endDate: string;
+  addLines: number;
+  removeLines: number;
+  totalLines: number;
+  submitTimes: number;
+  projectName: string;
+  username: string;
+  gitUsername: string;
+}
