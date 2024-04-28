2d7ee35501440e8616ba72015351fbaad5a9c7c9
YAN7 <904394373@qq.com>
2024-04-28 17:24:49 +0800

    feat:安装mysql依赖

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GitStatModule } from './git-stat/git-stat.module';
 
 @Module({
-  imports: [GitStatModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 3306,
+      username: 'common_user',
+      password: 'Aa3211',
+      database: 'git_stat',
+      entities: [],
+      // synchronize: true 不应在生产中使用，否则可能会丢失数据
+      synchronize: false,
+    }),
+    GitStatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
